20d560d28dd565060c601fb741e4b0a7f79a5fa3
elfwine <ascarrasca@gmail.com>
2020-03-16 16:32:23 +0900

    Add demo

---
+++ demo/precache-manifest.05c74d7134c82cc7e2220bf3a2c669b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5950cb0ac5c80121bb0e",
+    "url": "/js/chunk-vendors.baa6a421.js"
+  },
+  {
+    "revision": "8eb33d18a3e38ef83e9b",
+    "url": "/js/app.b1f8b1cd.js"
+  },
+  {
+    "revision": "c4ece061f44bcb0f09617f4d91cbd3c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c3dbefb55b447cd6d389b02fe43393e",
+    "url": "/fonts/DSEG7ClassicMini-Bold.woff2"
+  },
+  {
+    "revision": "c3fca09b182279382af5660116becda2",
+    "url": "/fonts/DSEG7ClassicMini-Bold.woff"
+  },
+  {
+    "revision": "8eb33d18a3e38ef83e9b",
+    "url": "/css/app.858627b2.css"
+  },
+  {
+    "revision": "958974918b3b46a92a98f660da82b0ed",
+    "url": "/buzzer.mp3"
+  }
+];
