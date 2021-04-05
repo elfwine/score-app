2584c30fd96f186c9bfe6bf8e787e5da50c33e96
elfwine <ascarrasca@gmail.com>
2021-04-05 15:07:35 +0900

    Update path

--- demo/precache-manifest.05c74d7134c82cc7e2220bf3a2c669b3.js
+++ demo/precache-manifest.05c74d7134c82cc7e2220bf3a2c669b3.js
@@ -5,30 +5,30 @@ self.__precacheManifest = [
   },
   {
     "revision": "5950cb0ac5c80121bb0e",
-    "url": "/js/chunk-vendors.baa6a421.js"
+    "url": "/score-app/demo/js/chunk-vendors.baa6a421.js"
   },
   {
     "revision": "8eb33d18a3e38ef83e9b",
-    "url": "/js/app.b1f8b1cd.js"
+    "url": "/score-app/demo/js/app.b1f8b1cd.js"
   },
   {
     "revision": "c4ece061f44bcb0f09617f4d91cbd3c8",
-    "url": "/index.html"
+    "url": "/score-app/demo/index.html"
   },
   {
     "revision": "0c3dbefb55b447cd6d389b02fe43393e",
-    "url": "/fonts/DSEG7ClassicMini-Bold.woff2"
+    "url": "/score-app/demo/fonts/DSEG7ClassicMini-Bold.woff2"
   },
   {
     "revision": "c3fca09b182279382af5660116becda2",
-    "url": "/fonts/DSEG7ClassicMini-Bold.woff"
+    "url": "/score-app/demo/fonts/DSEG7ClassicMini-Bold.woff"
   },
   {
     "revision": "8eb33d18a3e38ef83e9b",
-    "url": "/css/app.858627b2.css"
+    "url": "/score-app/demo/css/app.858627b2.css"
   },
   {
     "revision": "958974918b3b46a92a98f660da82b0ed",
-    "url": "/buzzer.mp3"
+    "url": "/score-app/demo/buzzer.mp3"
   }
 ];
